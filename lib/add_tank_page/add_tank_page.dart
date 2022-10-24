747577029b09ba1d0fb6affd3affa70ab982ff76
Nataly Salazar <natalysalazarglez@outlook.com>
2022-10-24 16:44:19 -0500

    feat: initial file structure

---
+++ lib/add_tank_page/add_tank_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTankPage extends StatelessWidget {
+  const AddTankPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/tank_detail_page/tank_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TankList extends StatelessWidget {
+  const TankList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
